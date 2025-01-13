b8dd8a834c862f817b253c5f2fc9986114481aab
gagan <gagan.singh@alertdriving.com>
2025-01-13 12:05:04 -0500

    adding chnages for chirs

---
+++ change_pass_chris.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IP_LIST='ip.txt'
+NEW_PASSWORD='sonic_root'
+SSH_USER='root'
+
+while read -r IP; do
+  if [[ -n "$IP" ]]; then
+    echo "Connecting to $IP..."
+    ssh  -o StrictHostKeyChecking=no "$SSH_USER@$IP" <<EOF
+      echo "root:$NEW_PASSWORD" | chpasswd
+EOF
+    if [ $? -eq 0 ]; then
+      echo "Root password changed successfully on $IP."
+    else
+      echo "Failed to change root password on $IP."
+    fi
+  fi
+done < "$IP_LIST"
+
+echo "Password change process completed."
